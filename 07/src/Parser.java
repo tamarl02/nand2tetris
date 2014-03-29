cc0cdfd6ceb53a5bfad48d50c6b4de95af85d886
NITZAN WERBER <nitzan.werber@gmail.com>
2014-03-29 20:07:53 +0300

    parser module 2

--- 07/src/Parser.java
+++ 07/src/Parser.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 
 public class Parser {
 	private static final String COMMENT = "//";
+	private static final Object CALL = "call";
+	private static final Object PUSH = "push";
+	private static final Object POP = "pop";
+	private static final Object FUNCTION = "function";
+	private static final Object IF_GOTO = "if-goto";
+	private static final Object RETURN = "return";
+	private static final Object LABEL = "label";
 
 	public enum Command {
 		C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL
@@ -13,6 +20,8 @@ public class Parser {
 	private BufferedReader _br = null;
 	private String _currentLine;
 	private String _currCommand;
+	private String _arg1;
+	private int _arg2;
 
 	/**
 	 * opens the INPUT stream/file and gets ready to parse it
@@ -34,7 +43,8 @@ public class Parser {
 		try {
 
 			_currentLine = _br.readLine();
-			// if there is a current line, but it's a comment or a space-line , keep reading the file
+			// if there is a current line, but it's a comment or a space-line ,
+			// keep reading the file
 			if (_currentLine != null) {
 				while (_currentLine.startsWith(COMMENT)
 						|| _currentLine.trim().length() == 0) {
@@ -75,6 +85,40 @@ public class Parser {
 	 *         all the arithmetic commands
 	 */
 	public Parser.Command commandType() {
+		String[] spiltLine = _currCommand.split(" ");
+
+		if (spiltLine.length == 1) {
+			_arg1 = spiltLine[0];
+			return Command.C_ARITHMETIC;
+		}
+		if (spiltLine.length > 1) {
+			_arg1 = spiltLine[1];
+		}
+		if (spiltLine.length > 2) {
+			_arg2 = Integer.parseInt(spiltLine[2]);
+		}
+		String command = _arg1 = spiltLine[0];
+		if (command.equals(CALL)) {
+			return Command.C_CALL;
+		}
+		if (command.equals(PUSH)) {
+			return Command.C_PUSH;
+		}
+		if (command.equals(POP)) {
+			return Command.C_POP;
+		}
+		if (command.equals(FUNCTION)) {
+			return Command.C_FUNCTION;
+		}
+		if (command.equals(IF_GOTO)) {
+			return Command.C_IF;
+		}
+		if (command.equals(RETURN)) {
+			return Command.C_RETURN;
+		}
+		if (command.equals(LABEL)) {
+			return Command.C_LABEL;
+		}
 		return null;
 
 	}
@@ -85,7 +129,7 @@ public class Parser {
 	 *         NOT BE CALLED IF: commandType() == C_RETURN
 	 */
 	public String arg1() {
-		return null;
+		return _arg1;
 
 	}
 
@@ -94,7 +138,7 @@ public class Parser {
 	 *         ONLY IF: commandType() == C_PUSH/C_POP/C_FUNCTION/C_CALL
 	 */
 	public int arg2() {
-		return 0;
+		return _arg2;
 
 	}
 }
